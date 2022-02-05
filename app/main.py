8acf6d9d7304ef87ab3b420d925ddfe70445113c
Jansen A. Simanullang <jansen.simanullang@gmail.com>
2022-02-05 16:06:49 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,31 +1,11 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+from . import *
+from app.api import home, page, twoforms, unsplash, accordion
 
-from .library.helpers import *
-from app.routers import twoforms, unsplash, accordion
+API = FastAPI()
+API.mount("/static", StaticFiles(directory="res"), name="static")
 
-
-app = FastAPI()
-
-
-templates = Jinja2Templates(directory="templates")
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-app.include_router(unsplash.router)
-app.include_router(twoforms.router)
-app.include_router(accordion.router)
-
-
-@app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
-    data = openfile("home.md")
-    return templates.TemplateResponse("page.html", {"request": request, "data": data})
-
-
-@app.get("/page/{page_name}", response_class=HTMLResponse)
-async def show_page(request: Request, page_name: str):
-    data = openfile(page_name+".md")
-    return templates.TemplateResponse("page.html", {"request": request, "data": data})
+API.include_router(home.api)
+API.include_router(page.api)
+API.include_router(unsplash.api)
+API.include_router(twoforms.api)
+API.include_router(accordion.api)
